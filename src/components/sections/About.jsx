911245fdc1e41a9daf6ca9bc29700da0c93b3283
abid7 <abidshahriar7@gmail.com>
2020-08-12 02:26:00 +0600

    text

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function About() {
-  return <div className="section">about page</div>;
+  return <div className="section">hello there, i am Abid Shahriar</div>;
 }
 
 export default About;

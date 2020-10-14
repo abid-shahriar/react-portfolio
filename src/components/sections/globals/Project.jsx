ca5e72e75e690e86e24560520712d7d8ca9843dc
abid7 <abidshahriar7@gmail.com>
2020-10-14 09:35:21 +0600

    finalized the projects section and done responsive css

--- src/components/sections/globals/Project.jsx
+++ src/components/sections/globals/Project.jsx
@@ -1,18 +1,23 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
-function Project({ src, title, responsiveness, children }) {
+function Project({ src, title, responsiveness, children, code, demo }) {
   return (
     <div className="project">
       <img src={src} alt={src} />
-      <div className="desc">
-        <h3>{title}</h3>
-        <p>{responsiveness}</p>
-        <p>{children}</p>
-      </div>
-      <div className="btns">
-        <Link>Code</Link>
-        <Link>Demo</Link>
+      <div className="decs-and-btns">
+        <div className="desc">
+          <h3>{title}</h3>
+          <p>{responsiveness}</p>
+          <p>{children}</p>
+        </div>
+        <div className="btns">
+          <a href={code} target="_blank" rel="noopener noreferrer">
+            Code
+          </a>
+          <a href={demo} target="_blank" rel="noopener noreferrer">
+            Demo
+          </a>
+        </div>
       </div>
     </div>
   );
--- src/components/sections/projects/Projects.jsx
+++ src/components/sections/projects/Projects.jsx
@@ -6,6 +6,7 @@ import cssPosition from "../../../asset/img/css-position.jpg";
 import foodOClock from "../../../asset/img/foodOClock.jpg";
 import todoApp from "../../../asset/img/todoApp.jpg";
 import pigGame from "../../../asset/img/pig-game.jpg";
+import simplePhotoEditor from "../../../asset/img/simple-photo-editor.jpg";
 
 function Projects() {
   return (
@@ -18,8 +19,8 @@ function Projects() {
             src={analogClock}
             title="analog clock"
             responsiveness="full-responsive"
-            code="blah"
-            demo="blah"
+            code="https://github.com/abid-shahriar/analog-clock"
+            demo="https://abid-shahriar.github.io/analog-clock/"
           >
             An analog-clock with random color generator built with HTML, CSS and
             Vanilla-JavaScript.
@@ -29,8 +30,8 @@ function Projects() {
             src={foodOClock}
             title="food O'Clock"
             responsiveness="full-responsive"
-            code="blah"
-            demo="blah"
+            code="https://github.com/abid-shahriar/restaurant-website"
+            demo="https://food-o-clock.netlify.app/"
           >
             A simple restaurant website built with HTML, CSS &
             Vanilla-JavaScript. The food menu is generated from a food api.
@@ -40,8 +41,8 @@ function Projects() {
             src={pigGame}
             title="Pig Game"
             responsiveness="not-responsive"
-            code="blah"
-            demo="blah"
+            code="https://github.com/abid-shahriar/pig-game"
+            demo="https://abid-shahriar.github.io/pig-game"
           >
             pig game with a dice. built with HTML, CSS & Vanilla-JavaScript and
             you can set the winnig score.
@@ -51,8 +52,8 @@ function Projects() {
             src={cssPosition}
             title="CSS Position GUI"
             responsiveness="full-responsive"
-            code="blah"
-            demo="blah"
+            code="https://github.com/abid-shahriar/react-css-position-gui"
+            demo="https://abid-shahriar.github.io/react-css-position-gui"
           >
             Created with React. Used react-hooks and contexApi with UseContex
             for satate-management.
@@ -62,22 +63,22 @@ function Projects() {
             src={todoApp}
             title="ToDo App"
             responsiveness="full-responsive"
-            code="blah"
-            demo="blah"
+            code="https://github.com/abid-shahriar/toDo-app"
+            demo="https://abid-shahriar.github.io/toDo-app/"
           >
             Simple To-Do app built with HTML, CSS and Vanilla-JavaScript with
             local storage support.
           </Project>
 
           <Project
-            src={analogClock}
-            title="analog clock"
+            src={simplePhotoEditor}
+            title="Photo Editor"
             responsiveness="full-responsive"
-            code="blah"
-            demo="blah"
+            code="https://github.com/abid-shahriar/simple-photo-editor"
+            demo="https://abid-shahriar.github.io/simple-photo-editor/"
           >
-            An analog-clock with random color generator built with HTML, CSS and
-            Vanilla-JavaScript.
+            A photo editor built with react. It can generate a random image from
+            unsplash and the image can be edited with css filter manupulation.
           </Project>
         </div>
       </div>

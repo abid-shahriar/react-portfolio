4c7ada062fd260d921eb9f774d8a7d9feb91b5f5
abid7 <abidshahriar7@gmail.com>
2020-10-16 13:49:48 +0600

    Updates

---
+++ precache-manifest.c61d7169d9386f16eef4e29dd4f81303.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "974e51ed9bc16099ceae98d86928fa9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e718723188fdc580ef78",
+    "url": "/static/css/main.d817ac70.chunk.css"
+  },
+  {
+    "revision": "287bd07e71e34f3ad225",
+    "url": "/static/js/2.7f0c6ba7.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.7f0c6ba7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e718723188fdc580ef78",
+    "url": "/static/js/main.80822c22.chunk.js"
+  },
+  {
+    "revision": "5c345266ec983ff137f6",
+    "url": "/static/js/runtime-main.7d027ca3.js"
+  },
+  {
+    "revision": "58034e7d5df11baeebf32625b689427a",
+    "url": "/static/media/analogClock.58034e7d.jpg"
+  },
+  {
+    "revision": "a8fa4fa2d00ce513a9ee5da973a0156a",
+    "url": "/static/media/css-position.a8fa4fa2.jpg"
+  },
+  {
+    "revision": "45ab07bf1a8f431d802c597b40a59f32",
+    "url": "/static/media/foodOClock.45ab07bf.jpg"
+  },
+  {
+    "revision": "3d8e4bc295224043db1995eb2dc71df5",
+    "url": "/static/media/me-2.3d8e4bc2.jpg"
+  },
+  {
+    "revision": "61b54a8633c28ff08cddb7b5216f2247",
+    "url": "/static/media/portfolio.61b54a86.jpg"
+  },
+  {
+    "revision": "fdd31c73ee46b152c9868c583d6f2549",
+    "url": "/static/media/simple-photo-editor.fdd31c73.jpg"
+  },
+  {
+    "revision": "ab64e5cf97ec913a5ef514e9a995fc01",
+    "url": "/static/media/todoApp.ab64e5cf.jpg"
+  }
+]);

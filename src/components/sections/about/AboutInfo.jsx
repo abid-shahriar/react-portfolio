bcf2ad431ba356235b5d015cf799516c52303a60
abid7 <abidshahriar7@gmail.com>
2020-10-24 00:36:25 +0600

    modified some text on the about section

--- src/components/sections/about/AboutInfo.jsx
+++ src/components/sections/about/AboutInfo.jsx
@@ -10,12 +10,10 @@ function AboutInfo() {
       <div className="info">
         <h2>I am Abid Shahriar,</h2>
         <p>
-          I am a Web Designer and a front-end web developer. I'm very passionate
-          and dedicated to my work. With 2 years of experience as a Web Designer
-          and Front-End Developer, I have acquired the skills and knowledge
-          necessary to make a project a succes. I enjoy every step of my work,
-          from discussion to collaboration. I love clean code and I try my best
-          to write clean and reusable code.
+          I am a Web Designer and a front-end web developer. With 2 years of
+          experience as a Web Designer and Front-End Developer, I have acquired
+          the skills and knowledge necessary to make a project a succes.I love
+          clean code and I try my best to write clean and reusable code.
         </p>
       </div>
     </div>

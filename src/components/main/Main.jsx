6c25f3faa5b0905c13a607708d19329aa08ef44e
abid7 <abidshahriar7@gmail.com>
2020-10-26 03:53:44 +0600

    added react lazy loading

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,20 +1,23 @@
-import React from "react";
-import About from "../sections/about/About";
-import Blogs from "../sections/blogs/Blogs";
-import Contact from "../sections/contact/Contact";
-import Projects from "../sections/projects/Projects";
-import Skills from "../sections/skills/Skills";
+import React, { lazy, Suspense } from "react";
 import { Route, Switch } from "react-router-dom";
+
+const About = lazy(() => import("../sections/about/About"));
+const Blogs = lazy(() => import("../sections/blogs/Blogs"));
+const Contact = lazy(() => import("../sections/contact/Contact"));
+const Projects = lazy(() => import("../sections/projects/Projects"));
+const Skills = lazy(() => import("../sections/skills/Skills"));
 
 function Main() {
   return (
     <main id="main">
       <Switch>
-        <Route path="/" exact component={About} />
-        <Route path="/blogs" component={Blogs} />
-        <Route path="/contact" component={Contact} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/skills" component={Skills} />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Route path="/" exact component={About} />
+          <Route path="/blogs" component={Blogs} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/skills" component={Skills} />
+        </Suspense>
       </Switch>
     </main>
   );

5bbceacf17f64b9dbd980d4f152c875762910e23
abid7 <abidshahriar7@gmail.com>
2021-02-02 12:25:47 +0600

    about page done

--- src/components/sections/about/About.jsx
+++ src/components/sections/about/About.jsx
@@ -1,16 +1,20 @@
-import React from "react";
-import AboutInfo from "./AboutInfo";
-import StayConnected from "./StayConnected";
+import React from 'react';
+import AboutInfo from './AboutInfo';
+import StayConnected from './StayConnected';
 
 function About() {
-  return (
-    <div className="section about" id="about">
-      <div className="section-wrapper">
-        <AboutInfo />
-        <StayConnected />
-      </div>
-    </div>
-  );
+	return (
+		<div className='section about' id='about'>
+			<div className='section-wrapper'>
+				<AboutInfo />
+				<StayConnected />
+
+				<a className='cv-btn' href='/abid-cv.pdf'>
+					Download CV
+				</a>
+			</div>
+		</div>
+	);
 }
 
 export default About;

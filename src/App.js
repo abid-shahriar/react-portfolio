7d667ddf77547ffa1a53f268e53512d0c19d3d88
abid7 <abidshahriar7@gmail.com>
2021-02-02 14:43:21 +0600

    nav fixes

--- src/App.js
+++ src/App.js
@@ -1,45 +1,45 @@
-import React, { useState } from "react";
-import { withRouter } from "react-router-dom";
-import Nav from "./components/nav/Nav";
-import Main from "./components/main/Main";
-import Loading from "./components/fallbacks/Loading";
+import React, { useState } from 'react';
+import { withRouter } from 'react-router-dom';
+import Nav from './components/nav/Nav';
+import Main from './components/main/Main';
+import Loading from './components/fallbacks/Loading';
 
 function App() {
-  const [Toggle, setToggle] = useState(true);
-  const [Loaded, setLoaded] = useState(false);
+	const [Toggle, setToggle] = useState(false);
+	const [Loaded, setLoaded] = useState(false);
 
-  window.addEventListener("click", (e) => {
-    if (!Toggle) {
-      if (e.target.classList.contains("toggle-btn")) {
-        return;
-      } else {
-        setToggle(true);
-      }
-    }
-  });
+	window.addEventListener('click', (e) => {
+		if (!Toggle) {
+			if (e.target.classList.contains('toggle-btn')) {
+				return;
+			} else {
+				setToggle(true);
+			}
+		}
+	});
 
-  window.addEventListener("swiped", (e) => {
-    if (e.detail.dir === "right") {
-      setToggle(false);
-    } else if (e.detail.dir === "left") {
-      setToggle(true);
-    }
-  });
+	window.addEventListener('swiped', (e) => {
+		if (e.detail.dir === 'right') {
+			setToggle(false);
+		} else if (e.detail.dir === 'left') {
+			setToggle(true);
+		}
+	});
 
-  if (Loaded) {
-    return (
-      <>
-        <Nav Toggle={Toggle} setToggle={setToggle} />
-        <Main Toggle={Toggle} setToggle={setToggle} />
-      </>
-    );
-  } else {
-    setTimeout(() => {
-      setLoaded(true);
-    }, 1000);
+	if (Loaded) {
+		return (
+			<>
+				<Nav Toggle={Toggle} setToggle={setToggle} />
+				<Main Toggle={Toggle} setToggle={setToggle} />
+			</>
+		);
+	} else {
+		setTimeout(() => {
+			setLoaded(true);
+		}, 1000);
 
-    return <Loading />;
-  }
+		return <Loading />;
+	}
 }
 
 export default withRouter(App);
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,61 +1,61 @@
-import React from "react";
-import { Link, withRouter } from "react-router-dom";
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
 
 function Nav(props) {
-  let locationPath = props.location.pathname;
+	let locationPath = props.location.pathname;
 
-  const navItems = [
-    {
-      id: 1,
-      name: "about",
-      icon: "nav-icon fas fa-user-alt",
-      to: "/",
-    },
-    {
-      id: 2,
-      name: "skills",
-      icon: "nav-icon fas fa-cogs",
-      to: "/skills",
-    },
-    {
-      id: 3,
-      name: "projects",
-      icon: "nav-icon fas fa-briefcase",
-      to: "/projects",
-    },
-    {
-      id: 5,
-      name: "contact",
-      icon: "nav-icon fas fa-id-card",
-      to: "/contact",
-    },
-  ];
+	const navItems = [
+		{
+			id: 1,
+			name: 'about',
+			icon: 'nav-icon fas fa-user-alt',
+			to: '/'
+		},
+		{
+			id: 2,
+			name: 'skills',
+			icon: 'nav-icon fas fa-cogs',
+			to: '/skills'
+		},
+		{
+			id: 3,
+			name: 'projects',
+			icon: 'nav-icon fas fa-briefcase',
+			to: '/projects'
+		},
+		{
+			id: 5,
+			name: 'contact',
+			icon: 'nav-icon fas fa-id-card',
+			to: '/contact'
+		}
+	];
 
-  const handelToggle = (e) => {
-    props.setToggle(!props.Toggle);
-  };
+	const handelToggle = (e) => {
+		props.setToggle(!props.Toggle);
+	};
 
-  return (
-    <nav id="nav" className={props.Toggle ? "toggle nav" : "nav"}>
-      <div className="toggle-btn" onClick={handelToggle}>
-        <i className="fas fa-sort-up"></i>
-      </div>
+	return (
+		<nav id='nav' className={props.Toggle ? 'toggle nav' : 'nav'}>
+			<ul className='nav-menu'>
+				<div className='toggle-btn' onClick={handelToggle}>
+					<p></p>
+					<p></p>
+					<p></p>
+				</div>
+				<div className='menu-items'>
+					{navItems.map((item) => (
+						<li className={locationPath === item.to ? 'active-nav' : ''} key={item.id}>
+							<Link to={item.to}>
+								<i className={item.icon}></i>
+								{item.name}
+							</Link>
+						</li>
+					))}
+				</div>
+			</ul>
 
-      <ul className="nav-menu">
-        {navItems.map((item) => (
-          <li
-            className={locationPath === item.to ? "active-nav" : ""}
-            key={item.id}
-          >
-            <Link to={item.to}>
-              <i className={item.icon}></i>
-              {item.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
-
-      {/* <div className="footer-text">
+			{/* <div className="footer-text">
         <p>Visit my github profile</p>
 
         <a
@@ -66,8 +66,8 @@ function Nav(props) {
           Github
         </a>
       </div> */}
-    </nav>
-  );
+		</nav>
+	);
 }
 
 export default withRouter(Nav);

3dbf95b4cd1af7fb8bebd13347b032de1081920e
abid7 <abidshahriar7@gmail.com>
2021-02-02 11:43:18 +0600

    modified about me

--- src/components/sections/about/AboutInfo.jsx
+++ src/components/sections/about/AboutInfo.jsx
@@ -1,23 +1,18 @@
-import React from "react";
-import myPic from "../../../asset/img/me.jpg";
+import React from 'react';
+import myPic from '../../../asset/img/me.jpg';
 
 function AboutInfo() {
-  return (
-    <div className="about__info">
-      <div className="img">
-        <img src={myPic} alt="abid shahriar" height="150px" width="150px" />
-      </div>
-      <div className="info">
-        <h2>I am Abid Shahriar,</h2>
-        <p>
-          I am a Web Designer and a front-end web developer. With 2 years of
-          experience as a Web Designer and Front-End Developer, I have acquired
-          the skills and knowledge necessary to make a project a succes.I love
-          clean code and I try my best to write clean and reusable code.
-        </p>
-      </div>
-    </div>
-  );
+	return (
+		<div className='about__info'>
+			<div className='img'>
+				<img src={myPic} alt='abid shahriar' height='150px' width='150px' />
+			</div>
+			<div className='info'>
+				<h2>I am Abid Shahriar,</h2>
+				<p>I am a Web Designer and a front-end web developer with 2 years of experience. I like clean-reusable code and love to learn new things.</p>
+			</div>
+		</div>
+	);
 }
 
 export default AboutInfo;
